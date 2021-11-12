a60714b73789550910a663a7c77d648de740e4dd
Dmitry <93593328+Keysto913@users.noreply.github.com>
2021-11-13 01:46:14 +0300

    Add files via upload

---
+++ generatorrandnums.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+using namespace std;
+void main()
+{
+	setlocale(LC_ALL, "ru");
+
+	srand(time(NULL));
+
+	int const size = 10;
+	int arr[size];
+	bool alreadyGot;
+
+	for (int i = 0; i < size;)
+	{
+		alreadyGot = false;
+		int randomValue = rand() % 100;
+
+		for (int j = 0; j < i; j++)
+		{
+			if (arr[j] == randomValue)
+			{
+				alreadyGot = true;
+			}
+		}
+
+		if (alreadyGot != true)
+		{
+			arr[i] = randomValue;
+			i++;
+		}
+	}
+
+	for (int i = 0; i < size; i++)
+	{
+		cout << arr[i] << endl;
+	}
+}
